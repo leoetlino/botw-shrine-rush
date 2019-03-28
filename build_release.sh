cb8008753b6a5de7fedf12088449fd3019d1754b
Léo Lam <leo@innovatetechnologi.es>
2019-03-28 18:06:57 +0100

    build: Don't abort if build dirs don't exist

--- build_release.sh
+++ build_release.sh
@@ -8,7 +8,7 @@ ASSETS_DIR_SWITCH=build/assets_switch
 ASSETS_DIR_WIIU=build/assets_wiiu
 
 # Switch
-rm -r $ASSETS_DIR_SWITCH
+rm -r $ASSETS_DIR_SWITCH || true
 ./build.py -t switch --gamedata-dir ~/botw/switch-view/Pack/Bootup.pack/GameData/gamedata.ssarc
 botw-patcher -t switch ~/botw/romfs-1.5.0/ $ASSETS_DIR_SWITCH $PATCH_DIR_SWITCH
 
@@ -20,7 +20,7 @@ OVERLAYFS_PID=$!
 botw-contentfs ~/botw/wiiu-base ~/botw/wiiu-upd build/mnt-content &
 CONTENTFS_PID=$!
 
-rm -r $ASSETS_DIR_WIIU
+rm -r $ASSETS_DIR_WIIU || true
 ./build.py -t wiiu --gamedata-dir build/mnt-content/Pack/Bootup.pack/GameData/gamedata.ssarc
 botw-patcher -t wiiu build/mnt-overlay $ASSETS_DIR_WIIU $PATCH_DIR_WIIU
 cp $PATCH_DIR_WIIU/Pack/Bootup_EUen.pack $PATCH_DIR_WIIU/Pack/Bootup_USen.pack
